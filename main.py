8b5ae110b1082916f50cbb647e8fa11d19fa734a
vblajan <vblajan@sdl.com>
2025-01-15 13:19:16 +0200

    Added parser

--- main.py
+++ main.py
@@ -4,13 +4,16 @@ from openai import AzureOpenAI
 
 import project_parser
 
+#path to project, add up to main so it doesn't parse Test files
+path = "C:\\Users\\vblajan\\Desktop\\Work\\Public API\\lc-webhook-delivery-service\\src\\main"
+
 client = AzureOpenAI(
   azure_endpoint = os.environ["ENDPOINT"],
   api_key= os.environ["API_KEY"],
   api_version="2024-02-01"
 )
 
-content = project_parser.get_content()
+content = project_parser.get_content(path)
 
 response = client.chat.completions.create(
     messages=[{
--- project_parser.py
+++ project_parser.py
@@ -1,42 +1,66 @@
 import os
+import re
 
+def compile_content(location):
+    classes = []
+    controllers = ""
+    models = []
+    for folder in os.walk(location):
+        for filename in os.listdir(folder[0]):
+            if '.java' in filename:
+                classes.append(os.path.join(folder[0], filename))
+                if 'Controller' in filename:
+                    with open(os.path.join(folder[0], filename), 'r') as f:
+                        write = False
+                        for line in f:
+                            if '@RestController' in line:
+                                write = True
+                            if write:
+                                models = add_models(models, line, "\\(*\\).{")[0]
+                                controllers = controllers +  line
 
-# def parse_project(location):
-#         for filename in os.listdir(location):
-#             with open(os.path.join(location, filename), 'r') as f:
-#                 write = False
-#                 for line in f:
-#                     if()
-#     return
+    return controllers + get_models(models, classes)
 
-def get_content():
-    content = """
-        @RestController
-        @RequestMapping("/")
-        public class TestController {
+def add_models(models, line, terminal):
+    modified = False
+    if "extends" in line:
+        modified = True
+        models.append(re.split("(extends | {)", line)[2])
+    if re.match("^((private|public|protected)?\\s+)?.*\\s+(\\w+)" + terminal, line):
+        value = None
+        if '<' in line:
+            value = re.split('(<|>)', line)[2]
+        else:
+            if line.split()[0][0].isupper():
+                value = line.split()[0]
+            if line.split()[1][0].isupper():
+                value = line.split()[1]
+        if value and value not in models:
+            models.append(value)
+            modified = True
+    return models, modified
 
+def get_models(models, classes):
+    values = ""
+    models_history = models.copy()
+    while models:
+        model = models.pop()
+        for clas in classes:
+            if model + ".java" in clas:
+                with open(clas, 'r') as f:
+                        write = False
+                        for line in f:
+                            if 'class ' + model in line:
+                                write = True
+                            if write:
+                                models_history, modified = add_models(models_history, line, "(;| =)")
+                                if modified:
+                                    models.append(models_history[-1])
+                                values = values + line
+    return values
 
-            @GetMapping("/get")
-            public TestModel getTest() {
-                return new TestModel();
-            }
-        }
-
-        @Getter
-        @Setter
-        @NoArgsConstructor
-        public class TestModel {
-        private String id;
-        private TestSubModel testSubModel;
-        }
-
-        @Getter
-        @Setter
-        @NoArgsConstructor
-        public class TestSubModel {
-            private String name;
-        }
-
-        create a contract based on this classes
-        """
+def get_content(path):
+    content = compile_content(path)
+    content = content + "Create a yaml contract based on these classes"
+    print(content)
     return content
